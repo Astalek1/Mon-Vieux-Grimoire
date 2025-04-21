840626e6a7c0693ad1f0f324aa24b3f5c8735003
kévin goujon <goujon.kevin@hotmail.fr>
2025-04-21 17:10:15 +0200

    Sécurisation de la connexion MongoDB via dotenv + mise à jour des dépendances

--- Backend/app.js
+++ Backend/app.js
@@ -1,24 +1,47 @@
 const express = require('express');
-
 const app = express();
+const mongoose = require('mongoose');
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_CLUSTER}/?retryWrites=true&w=majority&appName=${process.env.MONGO_DB_NAME}`;
+
 
 app.use((req, res, next) => {
-  console.log('Requête reçue !');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
 });
 
-app.use((req, res, next) => {
-  res.status(201);
-  next();
-});
 
-app.use((req, res, next) => {
-  res.json({ message: 'Votre requête a bien été reçue !' });
-  next();
-});
+mongoose.connect(uri,
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-app.use((req, res, next) => {
-  console.log('Réponse envoyée avec succès !');
+app.use('/api/stuff', (req, res, next) => {
+  const stuff = [
+    {
+      _id: 'oeihfzeoi',
+      title: 'Mon premier livre pour grimoire',
+      description: 'Les infos de mon premier objet',
+      imageUrl: 'https://www.idboox.com/wp-content/uploads/2023/08/harry-potter-7-tomes-en-1-livre.jpg',
+      price: 4900,
+      userId: 'qsomihvqios',
+    },
+    {
+      _id: 'oeihfzeomoihi',
+      title: 'Mon deuxième livre',
+      description: 'Les infos de mon deuxième livre',
+      imageUrl: 'https://www.idboox.com/wp-content/uploads/2023/08/harry-potter-7-tomes-en-1-livre.jpg',
+      price: 2900,
+      userId: 'qsomihvqios',
+    },
+  ];
+  res.status(200).json(stuff);
 });
 
 module.exports = app;
