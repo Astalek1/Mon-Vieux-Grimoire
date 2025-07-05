aad2c1d146603dd4afebeb5e1f2dacf4b3f51168
kévin goujon <goujon.kevin@hotmail.fr>
2025-07-05 19:46:17 +0200

    Ajout config CORS backend

--- Backend/app.js
+++ Backend/app.js
@@ -1,35 +1,33 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const cors = require('cors');
+
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 
+// Autoriser uniquement le front GitHub Pages
+app.use(cors({
+  origin: 'https://astalek1.github.io'
+}));
+
 const bookRoutes = require('./routes/book');
 const userRoutes = require('./routes/user');
 
-
+// Connexion à MongoDB
 const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_CLUSTER}/?retryWrites=true&w=majority&appName=${process.env.MONGO_DB_NAME}`;
-
-
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  next();
-});
-
-
-mongoose.connect(uri,
-  { useNewUrlParser: true,
-    useUnifiedTopology: true })
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-
-
+// Routes
 app.use('/api/books', bookRoutes);
 app.use('/api/auth', userRoutes);
 app.use('/images', express.static('images'));
-module.exports = app; 
+
+module.exports = app;
