a5d9bdd9982c6dc51d1565d86f96d2c4eb72f67b
kévin goujon <goujon.kevin@hotmail.fr>
2025-09-09 16:49:19 +0200

    test cron

--- Backend/cron.js
+++ Backend/cron.js
@@ -3,14 +3,8 @@ const mongoose = require("mongoose");
 
 (async () => {
   try {
-    const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}` +
-      `@${process.env.MONGO_CLUSTER}/${process.env.MONGO_DB_NAME}` +
-      `?retryWrites=true&w=majority`;
-    await mongoose.connect(uri);
-
-    // Ping ultra-léger (crée une vraie activité côté Atlas)
+    await mongoose.connect(process.env.MONGODB_URI);
     await mongoose.connection.db.admin().command({ ping: 1 });
-
     console.log("Health DB OK");
     await mongoose.disconnect();
   } catch (err) {
